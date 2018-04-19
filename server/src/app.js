4da6113eabe72f98cc3a7f7d0f44203903dd6afa
garrychiao <g50905g@gmail.com>
2018-04-20 03:50:44 +0800

    added sequelize to connect to database

--- server/src/app.js
+++ server/src/app.js
@@ -3,16 +3,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')// shows restful request details
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}!`
-  })
-})
+require('./routes')(app)
 
-app.listen(process.env.PORT || 8081)
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`server started on port ${config.port}`)
+  })
---
+++ server/src/config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'vue_express_test',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'root',
+    options: {
+      dialect: process.env.DIALECT || 'mysql',
+      host: process.env.HOST || 'localhost',
+      storage: './vue_express_test.mysql',
+      operatorsAliases: false
+    }
+  }
+}
